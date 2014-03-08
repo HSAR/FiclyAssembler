04f851676eef22e65fac5a209fc1595c86474359
Shu Sam Chen <research.hyperspace@gmail.com>
2014-03-08 12:49:51 +0000

    Corrected newline escaping.

--- src/Story.java
+++ src/Story.java
@@ -97,7 +97,7 @@ public class Story {
             processedText = processedText.replace("</p>", "");
             // add line breaks at appropriate positions (regex used for security), and newline chars to readability.
             // quad backslashes needed to 1) escape java to "\\\\" 2) escape regex to "\\"
-            processedText = processedText.replaceAll("<br[\\s]*[/]*>", "\\\\\\\\ \\n");
+            processedText = processedText.replaceAll("<br[\\s]*[/]*>", "\\\\\\\\ \n");
             // escape # characters
             processedText = processedText.replace("#", "\\#");
             // escape {} characters
